9aa88ab375974f7a57067fb7fa7b7818b852df5a
lumi-a <151774547+lumi-a@users.noreply.github.com>
2024-10-03 14:54:23 +0200

    Add smoothing to Mandelbrot

--- src/mandelbrot.rs
+++ src/mandelbrot.rs
@@ -1,16 +1,19 @@
-use num::complex::Complex;
+use num::complex::Complex32;
 
-const MAXIMUM_ESCAPETIME: usize = 1000;
+const MAX_ITERATIONS: usize = 1000;
 
-/// Computes the escape time of a point in the Mandelbrot set,
-/// up to a limit of [`MAXIMUM_ESCAPETIME`].
-pub fn escape_time(c: Complex<f32>) -> usize {
-    let mut z = Complex { re: 0.0, im: 0.0 };
-    for i in 0..MAXIMUM_ESCAPETIME {
+/// Computes the escape time of a point in the Mandelbrot set
+/// with smoothing, up to a limit of [`MAXIMUM_ESCAPETIME`]
+/// iterations.
+pub fn escape_time(c: Complex32) -> f32 {
+    let mut z = Complex32::new(0.0, 0.0);
+    let mut z_norm_sqr = 0.0;
+    for i in 0..MAX_ITERATIONS {
         z = z * z + c;
-        if z.norm_sqr() > 4.0 {
-            return i;
+        z_norm_sqr = z.norm_sqr();
+        if z_norm_sqr > 4.0 {
+            return i as f32 - z_norm_sqr.log2();
         }
     }
-    MAXIMUM_ESCAPETIME
+    MAX_ITERATIONS as f32 - z_norm_sqr.log2()
 }
