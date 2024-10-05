8589d7f9882f9274ebd45137056c007c6f3fad72
lumi-a <151774547+lumi-a@users.noreply.github.com>
2024-10-05 13:13:48 +0200

    Better colour-smoothing

--- src/mandelbrot.rs
+++ src/mandelbrot.rs
@@ -1,19 +1,19 @@
-use num::complex::Complex32;
+use num::complex::{c32, Complex32};
 
-const MAX_ITERATIONS: usize = 1000;
+pub const MAX_ITERATIONS: usize = 1000;
 
 /// Computes the escape time of a point in the Mandelbrot set
 /// with smoothing, up to a limit of [`MAXIMUM_ESCAPETIME`]
 /// iterations.
 pub fn escape_time(c: Complex32) -> f32 {
-    let mut z = Complex32::new(0.0, 0.0);
-    let mut z_norm_sqr = 0.0;
+    let mut z = c32(0.0, 0.0);
+    let mut z_norm_sqr;
     for i in 0..MAX_ITERATIONS {
         z = z * z + c;
         z_norm_sqr = z.norm_sqr();
         if z_norm_sqr > 4.0 {
-            return i as f32 - z_norm_sqr.log2();
+            return i as f32 + 1.0 - z_norm_sqr.log2() / 2.0;
         }
     }
-    MAX_ITERATIONS as f32 - z_norm_sqr.log2()
+    MAX_ITERATIONS as f32
 }
